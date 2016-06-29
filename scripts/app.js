7ff9692f728189f7e94aa956f2514357ed1d987a
Mark Johnson <markjohnson303@gmail.com>
2016-06-29 16:07:29 -0600

    updating data structure, abandoning subclasses. fuck subclasses.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,6 @@
+$( document ).ready(function() {
+	console.log("Ready!");
+
 	function Estimate (theLineItemsList){
 		this.lineItemsList = theLineItemsList;
 	}
@@ -6,16 +9,36 @@
 		constructor: Estimate,
 		addToLineItemsList:function (lineItemToAdd){
 			this.lineItemsList.push(lineItemToAdd);
-		}
+		},
+		nextLineItemID:function (){
+			var nextID;
+			var listLength = this.lineItemsList.length;
+			if(listLength === undefined){
+				nextID = 0;
+			} else {
+				nextID = listLength;
+			}
+			return nextID;
+		},
 	};
 
-	newEstimate = new Estiamte({});
+	newEstimate = new Estimate([]);
 
-	function LineItem (theActive, theIdentifier, theQauntity, theOptions, theType, theBasePrice) {
-		this.active = theActive;
+	function LineItem (theIdentifier, theActiveState, theQauntity, theType, theBasePrice, theTieLength) {
 		this.identifier = theIdentifier;
+		this.active = theActiveState;
 		this.quantity = theQauntity;
-		this.options = theOptions;
 		this.type = theType;
 		this.basePrice = theBasePrice;
+		this.TieLength = theTieLength;
 	}
+
+
+	$("#addNecktie").click(function(){
+		console.log("Added a tie!");
+		id = newEstimate.nextLineItemID();
+		newLineItem = new LineItem(id, true, 1, "necktie", 45, "std");
+		newEstimate.addToLineItemsList(newLineItem);
+	});
+
+});
